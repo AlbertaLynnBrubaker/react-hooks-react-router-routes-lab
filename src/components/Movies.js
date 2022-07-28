cd678607faa05b7ba2d01494b3f1ec8e274fbcb8
Alberta Lynn Brubaker <findingalberta@gmail.com>
2022-07-28 17:57:49 -0500

    commit tree 299fd7765a9b19be2de9fbaaef6ae95f4885df9d

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,26 @@
 import React from "react";
 import { movies } from "../data";
+import {v4 as uuid} from "uuid"
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  
+
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {movies.map(movie => {
+        return (
+          <div key={uuid()}>
+            {movie.title}
+            <p>Runtime: {movie.time}mins</p>
+            <ul>
+              {movie.genres.map(genre => <li key={uuid()}>{genre}</li>)}
+            </ul>
+          </div>
+        )
+      })}
+    </div>
+  )
 }
 
 export default Movies;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,61 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+const linkStyles = {
+  display: "inline-block",
+  width: "Calc(25% - 36px)",
+  padding: "12px",
+  margin: "0 6px 6px",
+  backgroundColor: "salmon",
+  textDecoration: "none",
+  color: "white",
+};
+
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div className="navbar">
+      <NavLink 
+        to="/"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          backgroundColor: "black",
+        }}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/movies"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          backgroundColor: "black",
+        }}
+      >
+        Movies
+      </NavLink>
+      <NavLink
+        to="/directors"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          backgroundColor: "black",
+        }}
+      >
+        Directors
+      </NavLink>
+      <NavLink
+        to="/actors"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          backgroundColor: "black",
+        }}
+      >
+        Actors
+      </NavLink>
+    </div>
+  )
 }
 
 export default NavBar;
